e2d157c99a53265ba1498d3031b6bd3026075701
Ugnius Saulevičius <ugnius.saulevicius@gmail.com>
2024-09-13 23:14:02 +0300

    probably working initial game example

--- frontend/battleship/src/index.js
+++ frontend/battleship/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client'; // Update the import for ReactDOM
+import { WelcomeScreen } from './WelcomeScreen';
+import { Game } from './Game/Game.js';
+import { Header } from './Header';
 
+import './css/style.css';
+
+export const App = () => {
+  const [appState, setAppState] = useState('welcome'); // play or welcome
+
+  const startPlay = () => {
+    setAppState('play');
+  };
+
+  // Renders either Welcome Screen or Game
+  return (
+    <>
+      <Header />
+      {appState === 'play' ? <Game /> : <WelcomeScreen startPlay={startPlay} />}
+    </>
+  );
+};
+
+// Updated to use ReactDOM.createRoot()
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
