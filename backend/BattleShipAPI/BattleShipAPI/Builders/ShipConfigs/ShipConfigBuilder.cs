3532f9c0b74f673d863d8d06f18f4a0065bfe137
Gytis Užkuraitis <gytis.uzkuraitis@gmail.com>
2024-11-24 19:04:33 +0200

    template pattern

--- backend/BattleShipAPI/BattleShipAPI/Builders/ShipConfigs/ShipConfigBuilder.cs
+++ backend/BattleShipAPI/BattleShipAPI/Builders/ShipConfigs/ShipConfigBuilder.cs
@@ -1,3 +1,4 @@
+using BattleShipAPI.Enums;
 using BattleShipAPI.Models;
 
 namespace BattleShipAPI.Builders.ShipConfigs;
@@ -20,8 +21,71 @@ public abstract class ShipConfigBuilder
     public abstract ShipConfigBuilder AddGlowing();
     public abstract ShipConfigBuilder AddFragile();
 
+
+    public GameMode gameMode;
+    public bool glowing;
+    public bool shielded;
+    public bool mobile;
+    public bool fragile;
+
     public ShipConfig Build()
     {
+        if (gameMode == GameMode.Normal)
+        {
+            StartNormal();
+        }
+        else if (gameMode == GameMode.Rapid)
+        {
+            StartRapid();
+        }
+        else if (gameMode == GameMode.Mobility)
+        {
+            StartMobility();
+        }
+        if (shielded)
+        {
+            AddShield();
+        }
+        if (mobile)
+        {
+            AddMobility();
+        }
+        if (fragile)
+        {
+            AddFragile();
+        }
+        if (glowing)
+        {
+            AddGlowing();
+        }
+
+
         return shipConfig;
+    }
+
+    public ShipConfigBuilder SetGameMode(GameMode gameMode)
+    {
+        this.gameMode = gameMode;
+        return this;
+    }
+    public ShipConfigBuilder SetShield(bool shield)
+    {
+        this.shielded = shield;
+        return this;
+    }
+    public ShipConfigBuilder SetMobility(bool mobile)
+    {
+        this.mobile = mobile;
+        return this;
+    }
+    public ShipConfigBuilder SetGlowing(bool glow)
+    {
+        this.glowing = glow;
+        return this; 
+    }
+    public ShipConfigBuilder SetFragile(bool fragile)
+    {
+        this.fragile = fragile;
+        return this;
     }
 }
